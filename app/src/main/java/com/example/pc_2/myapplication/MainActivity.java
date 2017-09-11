56ea429feca7cfce5a5717877cbd6672981a5396
jungwuk <jeonguk.min@gmail.com>
2017-09-11 14:22:04 +0900

    first commit

---
+++ app/src/main/java/com/example/pc_2/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.pc_2.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv1 = (TextView)findViewById(R.id.tv1);
+
+        tv1.setText("헬로월드");
+    }
+}
